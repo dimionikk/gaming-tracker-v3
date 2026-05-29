c76b845f9d2d23835fc9fa280c3e3eb702f7edd9
dimionik <dimionik952@gmail.com>
2026-05-29 18:21:33 +0300

    fix: add pool_pre_ping to prevent connection drops

--- user-service/app/database.py
+++ user-service/app/database.py
@@ -1,7 +1,7 @@
 from app.config import setting
 from sqlalchemy.ext.asyncio import create_async_engine,async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
-engine=create_async_engine(setting.database_url)
+engine = create_async_engine(setting.database_url, pool_pre_ping=True)
 async_session_maker=async_sessionmaker(engine,expire_on_commit=False)
 
 class Base(DeclarativeBase):
