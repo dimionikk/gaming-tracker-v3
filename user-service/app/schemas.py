da5bf1d2cc3ea8eb21bc522e0f99ca86370eb914
dimionik <dimionik952@gmail.com>
2026-05-30 11:42:16 +0300

    feat:add email validation

--- user-service/app/schemas.py
+++ user-service/app/schemas.py
@@ -1,26 +1,38 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, field_validator
 from uuid import UUID
 from typing import Optional
 
 class UserCreate(BaseModel):
-    username:str
-    email:str
-    password:str
+    username: str
+    email: EmailStr
+    password: str
+
+    @field_validator("password")
+    def password_min_length(cls, v):
+        if len(v) < 8:
+            raise ValueError("Пароль має бути мінімум 8 символів")
+        return v
 
 class UserLogin(BaseModel):
-    email:str
-    password:str
+    email: EmailStr
+    password: str
 
 class UserResponse(BaseModel):
-    id:UUID
-    username:str
-    email:str
+    id: UUID
+    username: str
+    email: str
 
 class Token(BaseModel):
     access_token: str
     token_type: str
 
 class UserUpdate(BaseModel):
-    username:Optional[str]=None
-    email:Optional[str]=None
-    password:Optional[str]=None
+    username: Optional[str] = None
+    email: Optional[EmailStr] = None
+    password: Optional[str] = None
+
+    @field_validator("password")
+    def password_min_length(cls, v):
+        if v is not None and len(v) < 8:
+            raise ValueError("Пароль має бути мінімум 8 символів")
+        return v
